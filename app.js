dcb0f967e0ed6b293fc72d220a737a8eaadde3ef
mingQ28 <eo77i9s@naver.com>
2025-07-04 08:47:46 +0900

    add annotation

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const inputArea = document.getElementById("translate-box");
 //console.dir(inputArea);
 let timer = null;
 
-// 먼저 스택에 clearTimeout 들어오고 web api에 전달된 timer를 즉시 취소
+// 먼저 스택에 clearTimeout 들어오고 web api에 전달된 timer를 즉시 취소 => 1초 되기 전에 계속 입력해도 clear => 1초가 되면 실행
 // web api는 setTimeout 실행(등록)
 // 스택에 setTimeout 올라가고 콜백함수, 1000 web api로 전달
 // 1초 후에 콜백함수 큐에 저장
 // 스택이 비어있을 경우 함수실행
 inputArea.addEventListener("input", (e) => {
   //console.dir(e.target);
-  clearTimeout(timer);
-  timer = setTimeout(() => {
+  clearTimeout(timer); // 계속 입력할 경우 timer 취소
+  timer = setTimeout(() => {// 1초 동안 입력값이 없을 경우 실행
     console.log(e.target.value);
-  }, 1000);
+  }, 1000); 
 });
