e11f7dd6906566baa02c453b60a79898e21aa878
unknown <eo77i9s@naver.com>
2025-07-03 17:42:22 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// 언어 감지 api
+// 사용자의 입력을 받고 2초 이상 입력을 하지 않는다면 언어감지 api 실행
+// 사용자 입력 -> 입력 값 읽기 -> api를 통해 반환값 확인 -> 반환값 출력
+
+// 사용자의 입력을 어떻게 읽어오지
+// keydown 리스너를 통해 디바운스 함수(clearTimeout)
+// 입력값을 api에 요청
+
+const inputArea = document.getElementById('translate-box');
+console.dir(inputArea);
+let text = '';
+
+inputArea.addEventListener('keydown', (e) => {
+    console.log(e);
+    console.log(inputArea.textContent);
+    let timer = setTimeout(() => {
+        console.log(inputArea.textContent);
+    }, 1000);
+    text = inputArea.textContent;
+    clearTimeout(timer);
+});
+console.log(text);
+
+
+// const xhr = new XMLHttpRequest();
+// xhr.onload = () => {
+//     if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+//         const responseData = xhr.responseText; // responseText: 서버로부터 받은 응답 데이터
+//         const result = JSON.parse(responseData); // JSON 역직렬화
+//         console.log(result); 
+//     }
+
+// }
+
+// xhr.open('POST', '/dect');
